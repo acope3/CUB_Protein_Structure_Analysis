0a87dcbbdb31b7397550d0fd8875554b6b8376b8
acope3 <alexandercope0@gmail.com>
2020-06-13 10:39:57 -0400

    Cleaning up for Ritu

--- plotModelWGeomMean.R
+++
@@ -1,9 +0,0 @@
-library(AnaCoDa)
-
-genome <- initializeGenomeObject("Data/Yeast/complete_seq.fasta")
-dis.phi <- read.table("")
-parameter <- initializeParameterObject(genome,1,1,rep(1,length(genome)), split.serine = TRUE, mixture.definition = "allUnique", initial.expression.values = init_phi)
-model<- initializeModelObject(parameter,model="ROC")
-pdf("with_genome_.pdf",height=12,width=10)
-plotModel(model,genome,samples=1,mixture=1)
-dev.off()
